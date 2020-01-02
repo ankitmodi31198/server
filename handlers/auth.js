ac7ceab6db268867ec04c2e6ddaf1362637e9d1d
Ankit Modi <ankitmodi31198@gmail.com>
2020-01-02 15:47:37 +0530

    register user without password hashing

--- handlers/auth.js
+++ handlers/auth.js
@@ -1,16 +1,42 @@
 // local modules
 const db = require('../models')
 
-exports.registerUser = async(req, res, next) => {
-    try{
+exports.registerUser = async (req, res, next) => {
+    try {
         var newUserBody = req.body
 
         var newUser = {
-            
+            cred: {
+                username: newUserBody.username,
+                password: newUserBody.password
+            },
+            details: {
+                name: newUserBody.name,
+                contact: {
+                    mobile1: newUserBody.mobile1,
+                    mobile2: newUserBody.mobile2,
+                    email: newUserBody.email
+                },
+                dob: newUserBody.dob,
+                address: {
+                    street: newUserBody.street,
+                    area: newUserBody.area,
+                    pincode: newUserBody.pincode,
+                    city: newUserBody.city,
+                    state: newUserBody.state,
+                    country: newUserBody.country
+                }
+            },
+            urls: {
+                website: newUserBody.website,
+                youtube: newUserBody.youtube
+            }
         }
 
-        // await newUser.save()
-    
+        var user = new db.User(newUser)
+
+        await user.save()
+
         res.json({newUser, success: true, msg: 'Congratulations! you have registered successfully.'})
     } catch (error) {
         error.status = 400
